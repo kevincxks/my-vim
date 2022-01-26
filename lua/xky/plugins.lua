1d9089ac5a14d31ebff7a060603307394522300d
kevinkyxue <kevinkyxue@tencent.com>
2022-01-26 10:34:05 +0800

    indentline

--- lua/xky/plugins.lua
+++ lua/xky/plugins.lua
@@ -107,6 +107,8 @@ return packer.startup(function(use)
   use "ahmedkhalf/project.nvim"
 
   use 'lewis6991/impatient.nvim'
+
+  use "lukas-reineke/indent-blankline.nvim"
   -- Automatically set up your configuration after cloning packer.nvim
 -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
