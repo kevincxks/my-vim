993fb42ed8b910334c03adc986dbdbc8f8146d25
kevincxks <467245044@qq.com>
2022-02-19 23:04:48 +0800

    colorschme:tokyonight

--- lua/xky/plugins.lua
+++ lua/xky/plugins.lua
@@ -147,6 +147,7 @@ return packer.startup(function(use)
       }
     end
   }
+  use 'folke/tokyonight.nvim'
   -- Automatically set up your configuration after cloning packer.nvim
 -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
