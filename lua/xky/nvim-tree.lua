f7e736095452c2b81880aceef8f1201d919aec51
kevincxks <1162367203@qq.com>
2022-07-18 19:53:41 +0800

    fix: cmp

--- lua/xky/nvim-tree.lua
+++ lua/xky/nvim-tree.lua
@@ -45,10 +45,6 @@ nvim_tree.setup {
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
-  update_to_buf_dir = {
-    enable = true,
-    auto_open = true,
-  },
   diagnostics = {
     enable = true,
     icons = {
@@ -78,7 +74,6 @@ nvim_tree.setup {
     height = 30,
     hide_root_folder = false,
     side = "left",
-    auto_resize = true,
     mappings = {
       custom_only = false,
       list = {
